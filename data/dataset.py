79ee1c36fd52c55d312e246dd170d538dedd0c24
Eric Yao <ericyaohan@yahoo.com>
2026-07-01 18:51:37 -0400

    added dataloader

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,16 @@
 import torch
+import numpy as np
 from torch.utils.data import Dataset
 
 class Dataset(Dataset):
 
-    def __init__(self):
-        return
+    def __init__(self, file_path):
+        data = np.load(file_path)
+        self.boards = torch.from_numpy(data['boards'])
+        self.scores = torch.from_numpy(data['scores']).float()
 
+    def __len__(self):
+        return len(self.scores)
 
-    def __getitem__(self, key):
-        return "", ""
+    def __getitem__(self, index):
+        return self.boards[index], self.scores[index]
