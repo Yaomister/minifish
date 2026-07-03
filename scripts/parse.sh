a8a5966a4f465ec0514c2896e3353a97a196badf
Eric Yao <ericyaohan@yahoo.com>
2026-07-03 13:33:13 -0400

    added training scripts

---
+++ scripts/parse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=parser
+#SBATCH --partition=short
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16G
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/parse_%j.out
+
+
+cd $SLURM_SUBMIT_DIR
+mkdir -p training
+mkdir -p logs
+source .venv/bin/activate
+python3 -m data.parser
