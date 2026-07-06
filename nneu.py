5e55bb6a96f7afca8db1408725c4134fc87f99cb
Eric Yao <ericyaohan@yahoo.com>
2026-07-06 18:52:10 -0400

    fixed issue with embedding bag

--- nneu.py
+++ nneu.py
@@ -12,7 +12,7 @@ class NNEU(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.feature_extractor = nn.Linear(in_features=self.in_features, out_features=256)
+        self.feature_extractor = nn.EmbeddingBag(40961, 256, mode="sum", padding_idx=40960)
         self.fn1 = nn.Linear(2 * 256, 32)
         self.fn2 = nn.Linear(32, 32)
         self.fn3 = nn.Linear(32, 1)
@@ -22,10 +22,11 @@ class NNEU(nn.Module):
         """
         Pass the features through the model.
         """
+        p = perspective.unsqueeze(1)
         w = self.feature_extractor(white_features)
         b = self.feature_extractor(black_features)
         # the weights switch order corresponding to the different perspectives
-        x = torch.cat([w, b], dim=1) if perspective else torch.cat([b, w], dim=1)
+        x = torch.cat([torch.where(p, w, b),torch.where(p, w, b)], dim=1)
         x = self.relu(x)
         x = self.relu(self.fn1(x))
         x = self.relu(self.fn2(x))
